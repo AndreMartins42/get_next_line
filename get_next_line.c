019fb4fc0647e0e704c21bd4496f626b5196a682
André Martins <andremaartiins@gmail.com>
2024-11-18 17:42:08 +0000

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -4,69 +4,127 @@
 #include <stdlib.h>
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1024
+# define BUFFER_SIZE 1
 #endif
 
 size_t  ft_strlen(char *str);
-char    *ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char    *read_file(int  fd);
+char	*get_next_line(int fd);
 
-int main(void)
+
+int main()
 {
-    int         fd;
-    char        *linha;
-    ssize_t     bytes_lidos;
-    static char buffer[BUFFER_SIZE];
+	int		fd;
+	char	*result;
 
-    fd = open("teste.txt", O_RDONLY);
-    if (fd < 0)
-    {
-        printf("Erro ao abrir o arquivo\n");
-        return 1;
-    }
-    while ((bytes_lidos = read(fd, buffer, BUFFER_SIZE)) > 0)
-    {
-        int i = 0;
-        while (i < bytes_lidos)
-        {
-            if (buffer[i] == '\n')
-            {
-                buffer[i] = '\0';
-                //linha = ft_strjoin(linha, buffer);
-                printf("Linha lida: %s\n", buffer);
-                //free(linha);
-                break;
-            }
-            i++;
-        }
-    }
-    if (bytes_lidos < 0)
-        printf("Erro ao ler o arquivo\n");
-
-    close(fd);
-    return 0;
+	fd = open("teste.txt", O_RDONLY);
+	if (fd < 0)
+		return (0);
+	result = read_file(fd);
+	printf("%s\n", result);
+	free(result);
 }
+
+/* char *get_next_line(int fd)
+{
+	char	*result;
+
+	if (fd < 0)
+		return (NULL);
+	result = read_file(fd);
+	return(result);
+}  */
+
+char    *read_file(int  fd) // ESTA FUNCAO E SOMENTE UM READ E RETORNA UMA STRING DO TXT TODO DE ACORDO COM O BUFFER
+{
+	// preciso pegar todo o TXT e alocar em uma vari'avel, para ai sim verificar as linhas e trabalhar em cima dos \n"
+	ssize_t bytes_lidos;
+	char    *buffer;
+	char	*cache = NULL;
+
+	if (fd < 0)
+		return (NULL);
+	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (buffer == NULL)
+		return (NULL);
+	while ((bytes_lidos = read(fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		buffer[bytes_lidos] = '\0';
+		cache = ft_strjoin(cache, buffer);
+	}
+	free(buffer);
+	return (cache);
+}
+
 size_t ft_strlen(char *str)
 {
-    size_t i = 0;
-    while (str[i])
-        i++;
-    return i;
+	size_t i = 0;
+
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return i;
 }
-// Função para juntar duas strings
-/*char *ft_strjoin(char const *s1, char const *s2)
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-    size_t len_s1 = s1 ? ft_strlen((char *)s1) : 0;
-    size_t len_s2 = ft_strlen((char *)s2);
-    char *new = malloc(len_s1 + len_s2 + 1);
-    if (!new)
-        return NULL;
+	size_t	len_s;
+	size_t	len_d;
+	size_t	i;
+	size_t	j;
 
-    if (s1)
-        for (size_t i = 0; i < len_s1; i++)
-            new[i] = s1[i];
-    for (size_t i = 0; i < len_s2; i++)
-        new[len_s1 + i] = s2[i];
+	len_s = ft_strlen((char *)src);
+	len_d = ft_strlen((char *)dst);
+	if (size <= len_d)
+		return (len_s + size);
+	j = 0;
+	i = len_d;
+	while (src[j] && i < size - 1)
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (len_s + len_d);
+}
 
-    new[len_s1 + len_s2] = '\0';
-    return new;
-}*/
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	if (dst == NULL || src == NULL)
+		return (0);
+	if (size == 0)
+		return (ft_strlen((char *)src));
+	i = 0;
+	while (src[i] != '\0' && i < size - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen((char *)src));
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new;
+	size_t	len_s1;
+	size_t	len_s2;
+ 	
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	len_s1 = ft_strlen((char *)s1);
+	len_s2 = ft_strlen((char *)s2);
+	new = (char *)malloc(sizeof(char) * (len_s1 + len_s2) + 1);
+	if (new == NULL)
+		return (NULL);
+	ft_strlcpy(new, s1, len_s1 + 1);
+	ft_strlcat(new, s2, (len_s1 + len_s2) + 1);
+	return (new);
+}

20eeb17c376a83c21665540ea71aa53645919431
André Martins <andremaartiins@gmail.com>
2024-11-30 13:01:24 +0000

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anmendes <anmendes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 11:39:57 by anmendes          #+#    #+#             */
+/*   Updated: 2024/11/30 12:57:44 by anmendes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 10
 #endif
 
-/*int main(void)
+/* int main(void)
 {
 	int	fd;
 	char *result;
@@ -19,11 +31,12 @@
 	}
 	close(fd);
 	return (0);
-}*/
-char *get_next_line(int fd)
+} */
+
+char	*get_next_line(int fd)
 {
-	static char *buffer;
-	char *line;
+	static char	*buffer;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
@@ -40,7 +53,7 @@ char	*ft_line(char *buffer)
 	char	*line;
 	int		i;
 
-	if (!buffer)
+	if (buffer == NULL || buffer[0] == '\0')
 		return (NULL);
 	i = 0;
 	while (buffer[i] && buffer[i] != '\n')
@@ -52,7 +65,8 @@ char	*ft_line(char *buffer)
 	return (line);
 }
 
-char	*ft_next(char *buffer) {
+char	*ft_next(char *buffer)
+{
 	char	*next;
 	int		i;
 
@@ -61,10 +75,8 @@ char	*ft_next(char *buffer) {
 	i = 0;
 	while (buffer[i] && buffer[i] != '\n')
 		i++;
-	if (buffer[i] == '\0') {
-		free(buffer);
-		return (NULL);
-	}
+	if (buffer[i] == '\0')
+		return (free(buffer), NULL);
 	next = ft_strdup(buffer + i + 1);
 	if (next == NULL)
 		return (NULL);
@@ -72,37 +84,40 @@ char	*ft_next(char *buffer) {
 	return (next);
 }
 
-char	*read_file(int fd, char *cache) {
+char	*read_file(int fd, char *buffer)
+{
 	ssize_t	bytes_lidos;
-	char	*buffer;
+	char	*cache;
 	char	*temp;
 
-	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buffer) 
+	cache = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (cache == NULL)
 		return (NULL);
-	bytes_lidos = read(fd, buffer, BUFFER_SIZE);
-	while (bytes_lidos > 0) {
-		buffer[bytes_lidos] = '\0';
-		temp = ft_strjoin(cache, buffer);
-		if (temp == NULL) {
-			free(buffer);
-			free(cache);
-			return (NULL);
-		}
-		free(cache);
-		cache = temp;
-		if (ft_strchr(buffer, '\n'))
-			break;
-		bytes_lidos = read(fd, buffer, BUFFER_SIZE);
+	bytes_lidos = read(fd, cache, BUFFER_SIZE);
+	while (bytes_lidos > 0)
+	{
+		cache[bytes_lidos] = '\0';
+		temp = ft_strjoin(buffer, cache);
+		if (temp == NULL)
+			return (free(cache), free(buffer), NULL);
+		free(buffer);
+		buffer = temp;
+		if (ft_strchr(cache, '\n'))
+			break ;
+		bytes_lidos = read(fd, cache, BUFFER_SIZE);
 	}
-	free(buffer); 
-	return (cache);
+	free(cache);
+	if (bytes_lidos < 0)
+		return (free(buffer), NULL);
+	return (buffer);
 }
 
-char	*ft_strchr(char *s, int c) {
+char	*ft_strchr(char *s, int c)
+{
 	if (s == NULL)
 		return (NULL);
-	while (*s) {
+	while (*s)
+	{
 		if (*s == (char)c)
 			return (s);
 		s++;
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,7 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anmendes <anmendes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 11:39:49 by anmendes          #+#    #+#             */
+/*   Updated: 2024/11/30 12:47:00 by anmendes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_strdup(char *s) {
+char	*ft_strdup(char *s)
+{
 	char	*cpy;
 	int		len_s;
 
@@ -15,67 +27,75 @@ char	*ft_strdup(char *s) {
 	return (cpy);
 }
 
-int	ft_strlen(char *str) {
-	int	i = 0;
+int	ft_strlen(char *str)
+{
+	int	i;
 
-	if (!str)
+	i = 0;
+	if (str == NULL)
 		return (0);
 	while (str[i])
 		i++;
 	return (i);
 }
 
-int	ft_strlcat(char *dst, char *src, int size) {
-	int	len_s = ft_strlen(src);
-	int	len_d = ft_strlen(dst);
-	int	i = len_d;
-	int	j = 0;
+int	ft_strlcat(char *dst, char *src, int size)
+{
+	int	len_s;
+	int	len_d;
+	int	i;
+	int	j;
 
+	len_s = ft_strlen(src);
+	len_d = ft_strlen(dst);
+	i = len_d;
+	j = 0;
 	if (size <= len_d)
 		return (len_s + size);
-	while (src[j] && i < size - 1) {
+	while (src[j] && i < size - 1)
 		dst[i++] = src[j++];
-	}
 	dst[i] = '\0';
 	return (len_s + len_d);
 }
 
-int	ft_strlcpy(char *dst, char *src, int size) {
+int	ft_strlcpy(char *dst, char *src, int size)
+{
 	int	i;
 	int	len_src;
 
-	len_src = ft_strlen((char *)src);
-	if (!dst || !src)
+	len_src = ft_strlen(src);
+	if (dst == NULL || src == NULL)
 		return (0);
 	if (size == 0)
 		return (len_src);
-	for (i = 0; i < size - 1 && src[i] != '\0'; i++)
+	i = 0;
+	while (src[i] != '\0' && i < size - 1)
+	{
 		dst[i] = src[i];
+		i++;
+	}
 	dst[i] = '\0';
 	return (len_src);
 }
 
-char *ft_strjoin(char *s1, char *s2) 
+char	*ft_strjoin(char *s1, char *s2)
 {
-    char *new;
-    int len_s1;
-    int len_s2;
+	char	*new;
+	int		len_s1;
+	int		len_s2;
 
-    if (!s1 && !s2)
-        return (NULL);
-    if (!s1)
-        return (ft_strdup(s2)); 
-    if (!s2)
-        return (ft_strdup(s1));
-    len_s1 = ft_strlen(s1);
-    len_s2 = ft_strlen(s2);
-    new = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
-    if (!new) {
-        free(s1);
-        return (NULL);
-    }
-    ft_strlcpy(new, s1, len_s1 + 1);
-    ft_strlcat(new, s2, len_s1 + len_s2 + 1);
-    return (new);
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	if (s1 == NULL)
+		return (ft_strdup(s2));
+	if (s2 == NULL)
+		return (ft_strdup(s1));
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	new = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+	if (new == NULL)
+		return (free(s1), NULL);
+	ft_strlcpy(new, s1, len_s1 + 1);
+	ft_strlcat(new, s2, len_s1 + len_s2 + 1);
+	return (new);
 }
-
